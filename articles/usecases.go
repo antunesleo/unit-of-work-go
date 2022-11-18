af17883d712fb1cd572f156159890003ffe5b58e
Leo Antunes <antunesleo4@gmail.com>
2022-11-18 10:49:14 -0300

    refactor create article

--- articles/usecases.go
+++ articles/usecases.go
@@ -21,32 +21,34 @@ func (a articleUseCases) CreateArticle(
 	ctx context.Context,
 	title, content, desc, categoryName string,
 ) (*Article, error) {
-	var article Article
-	article.Title = title
-	article.Content = content
-	article.Desc = desc
+	article := Article{
+		Title:   title,
+		Content: content,
+		Desc:    desc,
+	}
 
 	withinTx := func(uows UowStore) error {
 		categoryRepository := uows.GetCategoryRepository()
 		category, err := categoryRepository.FindByName(categoryName)
 
 		if err != nil {
-			if err == NotFoundError {
-				category = Category{
-					Name: categoryName,
-				}
-				addErr := categoryRepository.Add(&category)
-				if addErr != nil {
-					return addErr
-				}
-			} else {
+			if err != NotFoundError {
 				return err
+			}
+
+			category = Category{
+				Name: categoryName,
+			}
+			addErr := categoryRepository.Add(&category)
+			if addErr != nil {
+				return addErr
 			}
 		}
 
 		article.Category = category
 		return uows.GetArticleRepository().Add(&article)
 	}
+
 	err := a.uow.WithinTx(ctx, withinTx)
 	return &article, err
 }
